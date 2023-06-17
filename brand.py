0ae3a9449bc04da1dd604775c0a8438a5a499b05
Nageswara Rao Kolapalli <110399609+kvsnrkolapalli@users.noreply.github.com>
2023-06-17 20:35:12 +0530

    Update brand.py

--- brand.py
+++ brand.py
@@ -29,9 +29,9 @@ class Generic:
         else:
             # chromedriver_path = '/usr/bin/chromedriver'
             # self.browser=webdriver.Chrome('/usr/lib/chromium-browser/chromedriver', options=options)
-            # self.browser=webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=options)
+            self.browser=webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=options)
             # self.browser=webdriver.Chrome(executable_path='C:/Users/HAMZA/Downloads/chromedriver_win32/chromedriver.exe', options=options)
-            self.browser= webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#             self.browser= webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         self.browser.maximize_window()
 
     def open_page(self, url: str):
